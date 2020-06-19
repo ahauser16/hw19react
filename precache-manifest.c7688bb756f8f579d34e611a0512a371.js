5fb24273dcae5a659a77511b2e781c9cf5febef7
arthur hauser <arthur.hauser@gmail.com>
2020-06-19 17:48:33 -0400

    Updates

---
+++ precache-manifest.c7688bb756f8f579d34e611a0512a371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f1d959c34daa1f89458916fa480bc3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7ae3949d65f6b951c158",
+    "url": "/my-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d312b4684d313808a526",
+    "url": "/my-app/static/js/2.316dd0e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.316dd0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae3949d65f6b951c158",
+    "url": "/my-app/static/js/main.3716638c.chunk.js"
+  },
+  {
+    "revision": "619907742a1f139dae63",
+    "url": "/my-app/static/js/runtime-main.350abf92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
